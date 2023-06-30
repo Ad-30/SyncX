1b3e19e4b2c72015717d319d2bc11016f4cf19bc
Aditya <111758349+Ad-30@users.noreply.github.com>
2023-06-30 23:08:37 +0530

    Update Send.jsx

--- src/components/Send.jsx
+++ src/components/Send.jsx
@@ -34,7 +34,7 @@ function Send(){
     try {
       
       setFileSizeError(false)
-      const response = await fetch('http://ad30.pythonanywhere.com/upload/', {
+      const response = await fetch('/upload/', {
         method: 'POST',
         body: formData,
       });
